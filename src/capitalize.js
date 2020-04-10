e67e0e31f22ded27e91d9087684f6aa66cbc2141
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-10 17:19:29 -0400

    write capitalize function and add tests to it

---
+++ src/capitalize.js
@@ -0,0 +1,19 @@
+const capitalize = (str) => {
+  let result = '';
+  let previousAndThis;
+  str = str.split('');
+
+  str.forEach((char, i) => {
+    previousAndThis = str.slice(i - 1, i + 1).join('');
+
+    if (/\s[a-z]/.test(previousAndThis) || (/[a-z]/.test(char) && i === 0)) {
+      result += char.toUpperCase();
+    } else {
+      result += char;
+    }
+  });
+
+  return result;
+}
+
+export default capitalize;
